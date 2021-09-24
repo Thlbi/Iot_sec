a443fc400b1c373ffa14c9550b12219a7b715d50
thleroy3 <thleroy3@turing.local.isima.fr>
2021-09-24 10:44:45 +0200

    dylan est con

---
+++ programme_reference/Extraction des chaines de caractères du micrologiciel.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 18:14:38 2021
+
+@author: thiba
+"""
+import os
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Print the current working directory
+print("Current working directory: {0}".format(cwd))
+
+data=':10240400736F636F6765746563682F534543524503'
+
+def sum_words(data):
+    return sum(int(x, 16) for x in data)
+
+
+def checksum(data):
+    data2=[data[i:i+2] for i in range(1, len(data)-4, 2)] # on enlève les : + l'indicateur de fin de ligne + le checksum actuel
+    print(data2)
+    sum=sum_words(data2)
+    sum=(sum & 0x00FF)
+    sum2=(sum ^ 0x00FF)
+    CC= hex(sum2+1)
+    print(CC)
+    return CC[2:]
+
+def checksumFile(filename):
+    filin = open(filename,'r')
+    lignes = filin.readlines()
+    i_ligne = 0
+    donnees=[]
+    for ligne in lignes :
+        if ligne[8]=="0":
+            donnees.append(ligne[9:-3])
+        i_ligne += 1
+    print(donnees)
+    filin.close()
+    n=5
+
+    tab=[]
+    for i in range(len(donnees)):
+        nbcarac=0
+        for j in range(0,len(donnees[i],2)
+            val=donnees[j:j+2]
+        c.isprintable()
+#on prend en compte les caractères affichables (de 20 à 7D
+
+checksumFile('firmware_tp3_v2.33.hex')
+
+#checksum(data)
---
+++ programme_reference/code python pour les fichiers hex.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def calc_checksum(record):
+    """Returns the checksum of a record"""
+    record = [int(record[x:x+2],16) for x in range(1,len(record)-2,2)]
+    print(sum(record))
+    chksum = ((sum(record) ^ 0xff)+1) & 0xff
+    return chksum
+
+
+def replace_cheksum(record):
+    """Replace the checksum with the correct one"""
+    return record[:-2] + str(hex(calc_checksum(record))[2:]).upper()# [2:] to remove the "oX" in the hex value
+
+
+def decode(record):
+    """Displays each field of a record"""
+    if record[0] != ":":
+        raise Exception("Incorrect start code")
+    byte_count = record[1:3]
+    adress = record[3:7]
+    record_type = record[7:9]
+    data = ""
+    if record_type == "00": # if record == "01" then the data field is empty
+        data = record[9:9 + 2 * int(byte_count, base=16)]
+    checksum = record[-2:] # last byte
+
+    print(f"byte_count {byte_count}, adress {adress}, record_type {record_type}, data {data}, checksum {checksum}")
+
+
+def main():
+    record1 = ":1022F40047524F5550455F31340000003532313470"
+    print(replace_cheksum(record1))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
